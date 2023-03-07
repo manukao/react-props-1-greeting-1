bfa365682f0983981c58ab4b97824ee5e9dfbf06
Unam <97320894+manukao@users.noreply.github.com>
2023-03-07 15:36:15 +0100

    fix greeting component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./styles.css";
 
 export default function App() {
-  return <Greeting name="Jessica" />;
+  return (
+    <div>
+      <Greeting name="Jessica" />
+      <Greeting name="Dominik" />
+      <Greeting name="Manuel" />
+    </div>
+  );
 }
 
 function Greeting({ name }) {
-  return <p>Hello, {name === "Jessica" || "Dominik" ? "Coach" : name}!</p>;
+  return (
+    <p>Hello, {name === "Jessica" || name === "Dominik" ? "Coach" : name}!</p>
+  );
 }
